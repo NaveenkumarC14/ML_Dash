4d53e67b152a95fbb421e4e0b61ad7a9e352099e
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-08-13 11:14:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,19 +34,19 @@ petal_length = st.sidebar.text_input('Enter Height', '')
 
 petal_width =st.sidebar.text_input('Enter Weight', '')
         
-petal_width = st.sidebar.text_input('Enter ap_hi', '')
-petal_width = st.sidebar.text_input('Enter ap_lo', '')
+petal_width1 = st.sidebar.text_input('Enter ap_hi', '')
+petal_width2 = st.sidebar.text_input('Enter ap_lo', '')
         
-petal_width = st.sidebar.text_input('Enter cholesterol', '')
-petal_width = st.sidebar.text_input('Enter gluc', '')
-petal_width = st.sidebar.text_input('Enter smoke', '')
-petal_width =st.sidebar.text_input('Enter alco', '')
-petal_width = st.sidebar.text_input('Enter active', '')
+petal_width3 = st.sidebar.text_input('Enter cholesterol', '')
+petal_width4 = st.sidebar.text_input('Enter gluc', '')
+petal_width5 = st.sidebar.text_input('Enter smoke', '')
+petal_width6 =st.sidebar.text_input('Enter alco', '')
+petal_width7 = st.sidebar.text_input('Enter active', '')
         
         
         
        
-values = [sepal_length, sepal_width, petal_length, petal_width,petal_length,petal_length,petal_length,petal_length,petal_length,petal_length,petal_length]
+values = [sepal_length, sepal_width, petal_length, petal_width,petal_length1,petal_length2,petal_length3,petal_length4,petal_length5,petal_length6,petal_length7]
 
         
 if st.sidebar.button("Predict"):
